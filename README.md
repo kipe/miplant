f1c7122e0ff43c9313b0b3e9d21901437e1d89d8
Kimmo Huoman <kipenroskaposti@gmail.com>
2016-10-26 19:39:06 +0300

    Add some documentation.

---
+++ README.md
@@ -0,0 +1,35 @@
+# MiPlant #
+
+A Python-library for reading cheap plant monitoring sensors manufactured by
+Xiaomi.
+
+
+## Usage
+
+```python
+from miplant import MiPlant
+
+for miplant in MiPlant.discover():
+    print(miplant.temperature)
+```
+
+## Overview
+
+The sensors read 4 values:
+
+- temperature in degrees Celsius
+- light level in lux
+- moisture level in percent
+- conductivity in µS/cm (presented as "fertility" in many places, even the official app)
+
+The values are only read when the `MiPlant.read()` -function is called, or
+when one of the values is requested for the first time.
+
+## Notes
+
+- The sensors seem to have some kind of internal cache, so don't even bother reading them too frequently.
+
+## Thanks
+Big thanks goes to
+[Reverse engineering the Mi flora plant sensor](https://www.open-homeautomation.com/2016/08/23/reverse-engineering-the-mi-plant-sensor/)!
+I wouldn't have bothered with reverese engineering the messages myself :P
--- miplant/miplant.py
+++ miplant/miplant.py
@@ -17,6 +17,17 @@ class MiPlant(object):
         return "<MiPlant '%s'>" % (self.address)
 
     def read(self):
+        '''
+        Read the sensor with gattlib.
+
+        Returns
+        -------
+
+        success : boolean
+                  True if the data was successfully received, otherwise False.
+        '''
+        success = False
+
         requester = gattlib.GATTRequester(self.address, True, self.device)
         try:
             received_bytes = bytearray(requester.read_by_handle(0x35)[0])
@@ -24,30 +35,37 @@ class MiPlant(object):
             self._light = float(received_bytes[4] * 256 + received_bytes[3])
             self._moisture = received_bytes[7]
             self._conductivity = received_bytes[9] * 256 + received_bytes[8]
+            success = True
         except RuntimeError:
             self.log.exception('Failed to read sensor.')
         requester.disconnect()
 
+        return success
+
     @property
     def temperature(self):
+        ''' Get temperature value in degrees Celsius, calls read-function if read yet. '''
         if isnan(self._temperature):
             self.read()
         return self._temperature
 
     @property
     def light(self):
+        ''' Get light level in lux, calls read-function if read yet. '''
         if isnan(self._light):
             self.read()
         return self._light
 
     @property
     def moisture(self):
+        ''' Get moisture level in percent, calls read-function if read yet. '''
         if isnan(self._moisture):
             self.read()
         return self._moisture
 
     @property
     def conductivity(self):
+        ''' Get conductivity in µS/cm, calls read-function if read yet. '''
         if isnan(self._conductivity):
             self.read()
         return self._conductivity
