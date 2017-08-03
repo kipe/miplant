120ba9fc9f0c8e654957df44ad515b3f6424a639
Kimmo Huoman <kimmo.huoman@gef.fi>
2017-08-03 14:22:52 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='miplant',
-    version='0.1.0',
+    version='0.2.0',
     description='A library for reading cheap plant monitoring sensors manufactured by Xiaomi.',
     author='Kimmo Huoman',
     author_email='kipenroskaposti@gmail.com',
